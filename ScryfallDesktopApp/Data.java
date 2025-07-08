272b2f3bb470e42e76d2c42475c3ccaf7e29b001
mezzol2 <156289691+mezzol2@users.noreply.github.com>
2025-07-08 17:52:22 +0200

    Add files via upload

---
+++ ScryfallDesktopApp/Data.java
@@ -0,0 +1,7 @@
+import java.net.URI;
+import java.net.http.HttpRequest;
+
+//This is the Model file in the MVC
+public class Data {
+
+}
---
+++ ScryfallDesktopApp/ScryfallDesktop.java
@@ -0,0 +1,9 @@
+//This file is the control file in the MVC model
+public class ScryfallDesktop{
+    public static void main(String[] args) {
+        //Initialize the GUI and the API data
+        Data data = new Data();
+        GUI gui = new GUI();
+        
+    }
+}
